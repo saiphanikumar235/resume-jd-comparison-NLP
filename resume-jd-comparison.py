127100349c3e757c81fd3a072748a3c802ca5bf8
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-10-04 11:58:00 +0530

    Update resume-jd-comparison.py

--- resume-jd-comparison.py
+++ resume-jd-comparison.py
@@ -264,7 +264,7 @@ for uploaded_resume in uploaded_resumes:
         resume_details
     )
 if len(total_files) != 0:
-    res_df = st.table(pd.DataFrame(total_files).set_index(range(1, len(total_files)+1)))
+    res_df = st.table(pd.DataFrame(total_files).set_index(range(1, len(total_files)+1), inplace=True))
     st.download_button(
         "Click to Download",
         pd.DataFrame(total_files).to_csv(),
