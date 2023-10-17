475b9eac83decfab8f9144ee16442388db7d5b5d
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-10-17 16:48:01 +0530

    Update resume-jd-comparison.py

--- resume-jd-comparison.py
+++ resume-jd-comparison.py
@@ -132,6 +132,7 @@ def get_education(path, resume_text):
         res = get_details_from_openai(resume_text, 'what is the highest education degree give me in json format where key is degree')
         if res.startswith('{'):
             res = json.loads(res)
+            st.write(res)
             return res['education']
         return None
     else:
